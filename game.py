225621ada550b05f53f49ab49e29d62ec505b686
FabioFernandesCosta <fabio.costa@edu.unifil.br>
2021-04-17 00:20:38 -0300

    Base do jogo implementada
    No estado atual restam apenas pequenos ajustes

---
+++ game.py
@@ -0,0 +1,87 @@
+from pilha import pilha
+
+class game:
+    auxiliar, listaPilhas, nJogadas, nDiscos = None, None, None, None
+     
+    # inicializa as 3 pilhas de discos, e insere o numero desejado de discos na primeira pilha
+    def __init__(self, numeroDiscos:int):
+        self.nDiscos = numeroDiscos
+        self.listaPilhas = []
+        self.nJogadas = 0
+
+        for _ in range (3):
+            self.listaPilhas.append(pilha())
+        
+
+        for _ in reversed(range(numeroDiscos)):
+            self.listaPilhas[0].inserir(_)
+        pass
+
+
+    # tenta realizar a jogada, se não for possivel, a jogada é desfeita
+    def jogada(self, pilhaOrigem:int, pilhaObjetivo:int):
+        # retira disco da pilha de origem
+        self.auxiliar = self.listaPilhas[pilhaOrigem].remover()
+
+        # incrementa 1 ao numero de jogadas
+        self.nJogadas += 1
+
+        # tenta inserir disco na pilha objetivo, se não for possivel tenta devolver o disco a pilha origem
+        if not self.listaPilhas[pilhaObjetivo].inserir(self.auxiliar):
+            self.listaPilhas[pilhaOrigem].inserir(self.auxiliar)
+            # Remove 1 do numero de jogadas em caso de jogada invalida
+            self.nJogadas -= 1
+            pass
+
+        if game.condicaoVitoria(self.listaPilhas, self.nDiscos):
+            print("você venceu")
+        return([self.nJogadas, game.condicaoVitoria(self.listaPilhas, self.nDiscos)])
+        pass
+
+
+    def getAllPilhas(self):
+        allPilhas = []
+        for _ in range(3):
+            allPilhas.append(self.listaPilhas[_].pilhaArray)
+        return(allPilhas)
+        pass
+
+
+    def condicaoVitoria(listaPilhas, _nDiscos):
+        if (len(listaPilhas[2].pilhaArray) == _nDiscos):
+            return True
+        return False
+        pass
+
+    pass
+
+
+
+
+
+
+
+
+partida = game(3)
+print(partida.getAllPilhas())
+print(partida.jogada(0, 2))
+
+# jogada falha para testes
+print(partida.getAllPilhas())
+print(partida.jogada(0, 2))
+
+print(partida.getAllPilhas())
+print(partida.jogada(0, 1))
+print(partida.getAllPilhas())
+print(partida.jogada(2, 1))
+print(partida.getAllPilhas())
+print(partida.jogada(0, 2))
+print(partida.getAllPilhas())
+print(partida.jogada(2, 1))
+print(partida.getAllPilhas())
+print(partida.jogada(1, 0))
+print(partida.getAllPilhas())
+print(partida.jogada(1, 2))
+print(partida.getAllPilhas())
+print(partida.jogada(0, 2))
+print(partida.getAllPilhas())
--- pilha.py
+++ pilha.py
@@ -13,10 +13,16 @@ class pilha:
         #retorna True ou False, para facilitar o controle sobre as pilhas
             #EX: se o disco não puder ser inserido será retornado false para para que o manipulador possa devolver o disco ao seu lugar original
     def inserir(self, disco:int):
-        if pilha.isJogadaValida(disco) or not self.pilhaArray:
+        if not self.pilhaArray:
             self.topo += 1
             self.pilhaArray.append(disco)
             return True
+
+        elif disco < self.pilhaArray[self.topo]:
+            self.topo += 1
+            self.pilhaArray.append(disco)
+            return True
+
         else:
             # jogada jogada não será feita
             # avisar jogada invalida
@@ -27,13 +33,16 @@ class pilha:
     # remove o disco que esta no topo
     def remover(self):
         if self.pilhaArray:
-            self.pilhaArray.pop(self.topo)
+            discoRemovido = self.pilhaArray.pop(self.topo)
             self.topo -= 1
+            return discoRemovido
         else:
             print('ERROR: P1')
             # avisar erro pois isso não deveria ser chamado se a pilha de discos estiver vazia
             print
             pass
+        
+
 
 
     # descobre o topo
@@ -45,11 +54,5 @@ class pilha:
         return self.pilhaArray[self.topo]
 
 
-    # verifica se a jogada é valida
-    def isJogadaValida(self, disco) -> bool:
-        if disco < self.pilhaArray[self.topo]:
-            return True
-            
-        return False
-        pass
-
+    
+        
