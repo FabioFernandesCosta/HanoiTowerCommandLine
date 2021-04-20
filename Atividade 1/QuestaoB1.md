dac563b67bdb3dfad32e06e43f7403198ecbf377
ProsperoR <prospero@edu.unifil.br>
2021-04-20 15:52:59 -0300

    Questão B1 Concluida
    #7

---
+++ Atividade 1/QuestaoB1.md
@@ -0,0 +1,25 @@
+Estado inicial
+
+Torres = [
+    [3,2,1],
+    [0,0,0],
+    [0,0,0]
+]
+
+
+Estado intermediário aleatório
+
+Torres = [
+    [3,1,0],
+    [0,0,0],
+    [2,0,0]
+]
+
+
+Estado objetivo
+
+Torres = [
+    [0,0,0],
+    [0,0,0],
+    [3,2,1]
+]
--- game.py
+++ game.py
@@ -1,34 +1,34 @@
-from pilha import pilha
+from torre import torre
 
 class game:
-    auxiliar, listaPilhas, nJogadas, nDiscos = None, None, None, None
+    auxiliar, torres, nJogadas, nDiscos = None, None, None, None
      
-    # inicializa as 3 pilhas de discos, e insere o numero desejado de discos na primeira pilha
+    # inicializa as 3 pilhas de discos, e insere o numero desejado de discos na primeira torre
     def __init__(self, numeroDiscos:int):
         self.nDiscos = numeroDiscos
-        self.listaPilhas = []
+        self.torres = []
         self.nJogadas = 0
 
         for _ in range (3):
-            self.listaPilhas.append(pilha(numeroDiscos))
+            self.torres.append(torre(numeroDiscos))
         
         for _ in reversed(range(numeroDiscos)):
-            self.listaPilhas[0].inserir(_)
+            self.torres[0].inserir(_)
 
 
     # tenta realizar a jogada, se não for possivel, a jogada é desfeita
     def jogada(self, pilhaOrigem:int, pilhaObjetivo:int):
-        # retira disco da pilha de origem
-        self.auxiliar = self.listaPilhas[pilhaOrigem].remover()
+        # retira disco da torre de origem
+        self.auxiliar = self.torres[pilhaOrigem].remover()
 
         if self.auxiliar != -1:
             
             # incrementa 1 ao numero de jogadas
             self.nJogadas += 1
             foiAnulado = False
-            # tenta inserir disco na pilha objetivo, se não for possivel tenta devolver o disco a pilha origem
-            if not self.listaPilhas[pilhaObjetivo].inserir(self.auxiliar):
-                self.listaPilhas[pilhaOrigem].inserir(self.auxiliar)
+            # tenta inserir disco na torre objetivo, se não for possivel tenta devolver o disco a torre origem
+            if not self.torres[pilhaObjetivo].inserir(self.auxiliar):
+                self.torres[pilhaOrigem].inserir(self.auxiliar)
                 # Remove 1 do numero de jogadas em caso de jogada invalida
                 print("Jogada invalida um disco não pode ser colocado ejm cima de um disco de valor menor")
                 self.nJogadas -= 1
@@ -36,17 +36,17 @@ class game:
 
             if game.condicaoVitoria():
                 print("você venceu")
-        return([self.nJogadas, game.condicaoVitoria(self.listaPilhas, self.nDiscos)])
+        return([self.nJogadas, game.condicaoVitoria(self.torres, self.nDiscos)])
 
 
     def getAllPilhas(self):
         allPilhas = []
         for _ in range(3):
-            allPilhas.append(self.listaPilhas[_].pilhaArray)
+            allPilhas.append(self.torres[_].pilhaArray)
         return(allPilhas)
 
 
     def condicaoVitoria(self):
-        if (len(self.listaPilhas[2].pilhaArray) == self.nDiscos):
+        if (len(self.torres[2].pilhaArray) == self.nDiscos):
             return True
         return False
